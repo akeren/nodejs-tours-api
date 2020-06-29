37647bab69740550860567d367ed621dd2453c26
Kater Akeren <akerenkater@gmail.com>
2020-06-29 04:12:31 +0100

    Set & limit number of requests from an IP

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./api/controllers/helpers/errorController');
+
 // IMPORT ROUTES
 const userRouter = require('./api/routes/userRoutes');
 const tourRouter = require('./api/routes/tourRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000,
+	max: 100,
+	message: 'Too many requests from this IP, try again in an hour!'
+});
+app.use('/api/', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
