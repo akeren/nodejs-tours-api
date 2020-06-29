b9c8b4d012ef3417635b2d6b3a8a8453865b8938
Kater Akeren <akerenkater@gmail.com>
2020-06-29 04:30:51 +0100

    Set security for HTTP headers using helmet

--- src/app.js
+++ src/app.js
@@ -1,36 +1,55 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./api/controllers/helpers/errorController');
-
-// IMPORT ROUTES
 const userRouter = require('./api/routes/userRoutes');
 const tourRouter = require('./api/routes/tourRoutes');
 
 const app = express();
 
-// GLOBAL MIDDLEWARES
+/*
+ ** GLOBAL MIDDLEWARES
+ */
+
+// set security for HTTP Headers
+app.use(helmet());
+
+// Logging development requests
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Limit the number of requests allowed from same API
 const limiter = rateLimit({
 	windowMs: 60 * 60 * 1000,
 	max: 100,
 	message: 'Too many requests from this IP, try again in an hour!'
 });
 app.use('/api/', limiter);
+
+// Body parser, reading data from the req.body
 app.use(express.json());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// ROUTES
+/*
+ ** ROUTES
+ */
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-// HANDLING UNHANDLED ROUTES
+/*
+ ** HANDLING UNHANDLED ROUTES
+ */
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this Server!`, 404));
 });
 
-// GLOBAL ERROR
+/*
+ ** GLOBAL ERROR
+ */
 app.use(globalErrorHandler);
 
 module.exports = app;
