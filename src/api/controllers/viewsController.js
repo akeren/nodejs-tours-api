cac6b5372ddabcd0ce3d386b5ff8dfe4201942a0
Kater Akeren <akerenkater@gmail.com>
2020-07-28 13:01:04 +0100

    Created the tour overview page

--- src/api/controllers/viewsController.js
+++ src/api/controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsyncErrors = require('./../../utils/catchAsyncError');
+// 20bf6b
+// 76c9ce
+exports.getOverview = catchAsyncErrors(async (req, res) => {
+	const tours = await Tour.find({});
+
 	res.status(200).render('overview', {
-		title: 'All tours!'
+		title: 'All tours',
+		tours
 	});
-};
+});
 
 exports.getTour = (req, res) => {
 	res.status(200).render('tour', {
